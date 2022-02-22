5a068005d5172f70693a83725b72709308b6b182
Rai Singh <rai.verma@live.com>
2022-02-22 17:36:53 +0530

    enum, structure, boxing

--- ConsoleApp2/ConsoleApp2/ArrayPrograms.cs
+++ ConsoleApp2/ConsoleApp2/ArrayPrograms.cs
@@ -1,67 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
-    class ArrayPrograms
-    {
-        static void Main()
-        {
-            //int[] a = new int[5];
-            //int[] b = new int[] {44,333,22 };
+//namespace ConsoleApp2
+//{
+//    class ArrayPrograms
+//    {
+//        static void Main()
+//        {
+//            //int[] a = new int[5];
+//            //int[] b = new int[] {44,333,22 };
 
-            //for (int i = 0; i < a.Length; i++)
-            //{
-            //    Console.WriteLine("Enter an element ");
-            //    a[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //Console.WriteLine("Your array is ");
-            //for (int i = 0; i < a.Length; i++)
-            //{
-            //    Console.WriteLine($"{a[i]}");
+//            //for (int i = 0; i < a.Length; i++)
+//            //{
+//            //    Console.WriteLine("Enter an element ");
+//            //    a[i] = Convert.ToInt32(Console.ReadLine());
+//            //}
+//            //Console.WriteLine("Your array is ");
+//            //for (int i = 0; i < a.Length; i++)
+//            //{
+//            //    Console.WriteLine($"{a[i]}");
 
-            //}
-            /*
-             1. Sum of array elements
-             2. Biggest element in array
-             3. Search an element in array (linear search, binary serach)
-             4. print array in reverse order
-             5. print prime numbers of array
-             */
-            // Matrix 
-            int[,] m = new int[3, 3];
-            int[,] n=new int[,]{{1,2,3},{1,2,3},{1,2,3}};
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.WriteLine("enter element");
-                    m[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("Your 3x3 matrix");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write($"{m[i,j]}\t");
-                }
-                Console.Write("\n");
-            }
-            /*
-             1. Transpose
-             2. Left Diognal
-             3. Right Diognal
-             4. Lower Triangle
-             5. Upper Triangle
-             6. Sum of 2 matrix
-             7. Produdct of 2 Matrix
+//            //}
+//            /*
+//             1. Sum of array elements
+//             2. Biggest element in array
+//             3. Search an element in array (linear search, binary serach)
+//             4. print array in reverse order
+//             5. print prime numbers of array
+//             */
+//            // Matrix 
+//            int[,] m = new int[3, 3];
+//            int[,] n=new int[,]{{1,2,3},{1,2,3},{1,2,3}};
+//            for (int i = 0; i < 3; i++)
+//            {
+//                for (int j = 0; j < 3; j++)
+//                {
+//                    Console.WriteLine("enter element");
+//                    m[i, j] = Convert.ToInt32(Console.ReadLine());
+//                }
+//            }
+//            Console.WriteLine("Your 3x3 matrix");
+//            for (int i = 0; i < 3; i++)
+//            {
+//                for (int j = 0; j < 3; j++)
+//                {
+//                    Console.Write($"{m[i,j]}\t");
+//                }
+//                Console.Write("\n");
+//            }
+//            /*
+//             1. Transpose
+//             2. Left Diognal
+//             3. Right Diognal
+//             4. Lower Triangle
+//             5. Upper Triangle
+//             6. Sum of 2 matrix
+//             7. Produdct of 2 Matrix
              
-             */
+//             */
 
-        }
-    }
-}
+//        }
+//    }
+//}
