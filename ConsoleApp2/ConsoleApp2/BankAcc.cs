cda35f16594268043039c678d73814f2c66cef7c
Rai Singh <rai.verma@live.com>
2022-02-24 17:19:46 +0530

    oops day 1

---
+++ ConsoleApp2/ConsoleApp2/BankAcc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class BankAcc
+    {
+        private int accno;
+        private string name;
+        private double bal;
+        private static int x = 1000;
+        // Property Unit - Used to give access of class members or variables outside the class
+        public BankAcc()
+        {
+            accno = ++x;
+            bal = 500;
+            name = "NA";
+        }
+        public BankAcc(string acname, double bal)
+        {
+            accno = ++x;
+            this.bal = bal;
+            name = acname;
+        }
+        public BankAcc(BankAcc acc)
+        {
+            accno = ++x;
+            bal = acc.bal;
+            name = acc.name;
+        }
+        public int AccountNo
+        {
+            get { return accno; }
+        }
+        public string AccName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Balance
+        {
+            get { return bal; }
+        }
+
+        public string Deposit(double amnt, string pan="")
+        {
+            if(amnt>=50000 && pan!="")
+            bal = bal + amnt;
+            else
+                return $"You can not Deposit. pan number is required ";
+
+            return $"Deposit Successfully after deposit your balance is {bal}";
+
+        }
+        public string Withdraw(double amnt)
+        {
+            if (amnt <= bal)
+                bal = bal - amnt;
+            else
+                return $"Not Sufficient Balanance Your balance is {bal} but you want to withdraw {amnt}";
+            
+            return $"Withdraw Successfully. after withdraw your bal is {bal}";
+        }
+
+
+    }
+}
--- ConsoleApp2/ConsoleApp2/EnumDemo.cs
+++ ConsoleApp2/ConsoleApp2/EnumDemo.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
-    // Type - Defined by user 
+//namespace ConsoleApp2
+//{
+//    // Type - Defined by user 
 
-    public enum WeekDay
-    {
-        Monday=1,
-        Tuesday=2,
-        Wednesday=3,
-        Thursday=4,
-        Friday=5,
-        Saturday=6,
-        Sunday=7
-    }
-    public class Book
-    {
+//    public enum WeekDay
+//    {
+//        Monday=1,
+//        Tuesday=2,
+//        Wednesday=3,
+//        Thursday=4,
+//        Friday=5,
+//        Saturday=6,
+//        Sunday=7
+//    }
+//    public class Book
+//    {
         
-        public string name;
-        public string author;
-        public string isbn;
-        public double price;
-        public int pubyear;
+//        public string name;
+//        public string author;
+//        public string isbn;
+//        public double price;
+//        public int pubyear;
 
-    }
-    class EnumDemo
-    {
-        static void Main()
-        {
-            int day;
-            Book book = new Book() {name="Let Us C",author="Yaswant Kanetkar",isbn="ABC123",price=290.44,pubyear=2006};
+//    }
+//    class EnumDemo
+//    {
+//        static void Main()
+//        {
+//            int day;
+//            Book book = new Book() {name="Let Us C",author="Yaswant Kanetkar",isbn="ABC123",price=290.44,pubyear=2006};
             
-            Book book1 = new Book();
-            book1.name = "C#.Net";
-            book1.price = 560.88;
-            book1.isbn = "XYZ123";
-            book1.pubyear = 2009;
+//            Book book1 = new Book();
+//            book1.name = "C#.Net";
+//            book1.price = 560.88;
+//            book1.isbn = "XYZ123";
+//            book1.pubyear = 2009;
 
-            // boxing - unboxing 
-            int num = 10;
-            object obj = book;
+//            // boxing - unboxing 
+//            int num = 10;
+//            object obj = book;
             
-            int num2 = (int)obj;
+//            int num2 = (int)obj;
 
-            Console.WriteLine("enter day of the week");
-            day = Convert.ToInt32(Console.ReadLine());
-            WeekDay x = (WeekDay)day;
-            Console.WriteLine($"Today is {x}");
-        }
-    }
-}
+//            Console.WriteLine("enter day of the week");
+//            day = Convert.ToInt32(Console.ReadLine());
+//            WeekDay x = (WeekDay)day;
+//            Console.WriteLine($"Today is {x}");
+//        }
+//    }
+//}
