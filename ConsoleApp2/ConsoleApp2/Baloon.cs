e574cd7226674863054955d0390e4dd5e1a2d427
Rai Singh <rai.verma@live.com>
2022-03-11 17:43:41 +0530

    baloon

---
+++ ConsoleApp2/ConsoleApp2/Baloon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Baloon
+    {
+        private delegate void BangDel(string msg);
+        private event BangDel Bang;
+        public delegate void PumpDel(object sender);
+        public event PumpDel Pump;
+       
+        public Baloon()
+        {
+            _dia = 2;
+            _pond = 1;
+            Bang += Baloon_Bang;
+        }
+
+        private int _dia;
+        
+
+        public int Dia
+        {
+            get { return _dia; }
+            set { _dia = value; }
+        }
+        private int _pond;
+
+        public int Pond
+        {
+           
+            get { return _pond; }
+            set { _pond = value; }
+        }
+
+        public void FillAir()
+        {
+            _pond++;
+            _dia = _pond * 2;
+            
+            Pump(this);
+            
+            if (_dia >= 5)
+                Bang($"Booooooooooooooooooom because dia {_dia}");
+              
+        }
+
+        private void Baloon_Bang(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+    class BaloonPrgram
+    {
+        static void Main()
+        {
+            Baloon baloon = new Baloon();
+            // create event handler
+            baloon.Pump += Baloon_Pump;
+            // raise event
+            baloon.FillAir();
+            baloon.FillAir();
+        }
+
+        private static void Baloon_Pump(object sender)
+        {
+            Baloon baloon = (Baloon)sender;
+            Console.WriteLine($"Baloon Dia {baloon.Dia} and air in baloon is {baloon.Pond} pond.");
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/ProgramDelegate.cs
+++ ConsoleApp2/ConsoleApp2/ProgramDelegate.cs
@@ -1,42 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
-    class ProgramDelegate
-    {
-        public delegate void Print(int value);
-       static void Main()
-        {
-            //Print print;
-            //print = PrintNumber;
-            //print(10000);
-            //print = PrintMoney;
-            //print(10000);
+//namespace ConsoleApp2
+//{
+//    class ProgramDelegate
+//    {
+//        public delegate void Print(int value);
+//       static void Main()
+//        {
+//            //Print print;
+//            //print = PrintNumber;
+//            //print(10000);
+//            //print = PrintMoney;
+//            //print(10000);
 
-            //PrintHelper(PrintNumber, 100000);
-            //PrintHelper(PrintMoney, 100000);
-            Print print = PrintNumber;
-            print += PrintMoney;
-            print(100000);
+//            //PrintHelper(PrintNumber, 100000);
+//            //PrintHelper(PrintMoney, 100000);
+//            Print print = PrintNumber;
+//            print += PrintMoney;
+//            print(100000);
 
-        }
-        static void PrintNumber(int num)
-        {
-            Console.WriteLine("Your number is {0,-12:N0}", num);
+//        }
+//        static void PrintNumber(int num)
+//        {
+//            Console.WriteLine("Your number is {0,-12:N0}", num);
 
-        }
-        static void PrintMoney(int money)
-        {
-            Console.WriteLine("Your Money is {0:C}", money);
+//        }
+//        static void PrintMoney(int money)
+//        {
+//            Console.WriteLine("Your Money is {0:C}", money);
 
-        }
-        static void PrintHelper(Print print, int num)
-        {
-            print(num);
-        }
-    }
-}
+//        }
+//        static void PrintHelper(Print print, int num)
+//        {
+//            print(num);
+//        }
+//    }
+//}
+///*
+// int dia=2;
+//public static readonly int maxpond =20;
+//FillAir() => it increse the pond with 2 
+
+//Raise an event => Bang
+//Raise an event => Motapa
+ 
+// */
