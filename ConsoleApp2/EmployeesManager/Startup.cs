b240db13ed7c09984c776dd0e3d684506734533f
Rai Singh <rai.verma@live.com>
2022-04-13 17:51:31 +0530

    pipilines complete

--- ConsoleApp2/EmployeesManager/Startup.cs
+++ ConsoleApp2/EmployeesManager/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,13 @@ namespace EmployeesManager
     public class Startup
     {
         private readonly IConfiguration configuration;
-
+      
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
+         
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -28,14 +30,50 @@ namespace EmployeesManager
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+           
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageOptions exceptionPageOptions = new DeveloperExceptionPageOptions()
+                { SourceCodeLineCount = 10};
+                app.UseDeveloperExceptionPage(exceptionPageOptions);
             }
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("foo.html");
+            app.UseDefaultFiles(options);
 
-            app.UseRouting();
+            app.UseStaticFiles();
+
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW1: Incoming Request");
+            //    await next();
+            //    logger.LogInformation("MW1: Outgoing Response");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW2: Incoming Request");
+            //    await next();
+            //    logger.LogInformation("MW2: Outgoing Response");
+            //});
+
+            app.Run(async (context) =>
+            {
+                //throw new Exception("Some error during processing");
+                await context.Response.WriteAsync($"Your Working Enviorment is {env.EnvironmentName}");
+
+                //logger.LogInformation("MW3: Accespting Request and Giving Response");
+            });
+
+        }
+    }
+}
+
+/*
+ app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
@@ -46,6 +84,4 @@ namespace EmployeesManager
                     $" \n {configuration["MyMsg"]}");
                 });
             });
-        }
-    }
-}
+ */
